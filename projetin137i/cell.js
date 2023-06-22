89e38a5fc9dc370c1247f749b0ca602fb6b59766
Ddanilo122 <112113175+Ddanilo122@users.noreply.github.com>
2023-06-22 14:30:09 -0300

    Add files via upload

---
+++ projetin137i/cell.js
@@ -0,0 +1,51 @@
+var ad=''
+
+function setup(){
+        canvas=createCanvas(500,300)
+        goku=ml5.objectDetector('cocossd',modelLoaded)
+    }
+    
+
+        function draw(){
+            canvas.position(windowWidth/2-250,200)
+            image(img,0,0,500,300)
+            fill('red')
+            if(detectado){
+                textSize(20)
+                stroke('#0066CC')
+                strokeWeight(3);
+                fill('#0066CC')
+                noFill()
+                for(var i in objeto){
+                    fill('#0066CC')
+                ad=objeto[i]  
+            text(ad.label,ad.x,ad.y+25)
+            noFill()
+            rect(ad.x,ad.y,ad.width,ad.height)
+                }
+            }
+        }
+         function preload(){
+                img=loadImage('1.png')
+            }
+
+           
+ 
+                function modelLoaded()
+                {
+                    console.log('KAMEKAMEKAMEHAAAAAAAAAAAAAA')
+                    goku.detect(canvas,gotR)
+
+                }
+                var objeto=[]
+                var detectado=false
+                function gotR(erro,result){
+                    if(erro){
+                        console.log(erro)
+                    }else{
+                     objeto=result
+                     detectado=true; 
+                    }
+                    
+
+                }
---
+++ projetin137i/fonte.js
@@ -0,0 +1,15 @@
+function ircell(){
+    window.location='cell.html'
+}
+function sair(){
+    window.location='index.html'
+}
+function irmoeda(){
+    window.location='moeda.html'
+}
+function ircopo(){
+   window.location='copo.html' 
+}
+function sair(){
+    window.location='index.html'
+}
